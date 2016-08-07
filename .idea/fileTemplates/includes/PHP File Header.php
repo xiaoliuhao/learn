ca3588fff17204223bb9b7190998fa41aca912e4
Liu <470401911@qq.com>
2016-08-07 20:05:56 +0800

    使用php实现简单的Hash表

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,7 @@
+/** 
+ * Create by ${PRODUCT_NAME}
+ * Author: ${USER}  <470401911@qq.com>
+ * Date: ${DATE}
+ * Time: ${TIME}
+ * Version: ${PROJECT_NAME}
+ */
---
+++ .idea/fileTemplates/includes/PHP Function Doc Comment.php
@@ -0,0 +1,7 @@
+/**
+${NAME}   ${THROWS_DOC}
+@access public 
+${PARAM_DOC}
+#if (${TYPE_HINT} != "void") * @return ${TYPE_HINT}
+#end
+*/
